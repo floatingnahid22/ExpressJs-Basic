4a6a0ecbfeb548eddf85ec9e01fffef8abd3c80e
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-20 16:41:17 +0600

    template engine

--- index.js
+++ index.js
@@ -111,13 +111,37 @@
 
 
 //app.route()
+// const express  = require('express');
+
+// const app = express();
+
+
+// app.route('/about/mission')
+//     .get((req, res) => {
+//         res.send('Welcome to application home get');
+//     })
+//     .post((req, res) => {
+//         res.send('Welcome to application home post');
+//     })
+//     .put((req, res) => {
+//         res.send('Welcome to application home put');
+//     });
+
+// app.listen(3000, () => {
+//     console.log('listening on port 3000');
+// });
+
+//template engine 
+//ejs
 const express  = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.route('/about/mission')
     .get((req, res) => {
-        res.send('Welcome to application home get');
+        res.render('pages/about');
     })
     .post((req, res) => {
         res.send('Welcome to application home post');
