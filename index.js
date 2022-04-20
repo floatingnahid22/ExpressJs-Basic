8c6646925109dcb64e42b5298204cd77f0fa5ac2
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-20 14:56:30 +0600

    understanding express() object

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express  = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+//express.static()
+// app.use(express.static(`${__dirname}/public/`, {
+//     index: "home.html",
+// })
+// );
+
+//express.router
+const router = express.Router({
+    caseSensitive: false,
+});
+app.use(router);
+
+app.get('/about', (req, res) => {
     res.send('this is home page');
 });
 
 app.post('/', (req, res) => {
+    // console.log(req.body);
     res.send('this is home page with post request');
 });
 
