0460c4c261ea62ab60bfb1fd27213ee87f5ee671
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-21 22:20:47 +0600

    error-handling pushed

--- index.js
+++ index.js
@@ -1,12 +1,78 @@
-const express = require("express");
-const adminRouter = require('./adminRouter');
-const publicRouter = require('./publicRouter');
+//synchronous code error handle
 
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   for (let i = 0; i <= 10; i++) {
+//     if (i === 5) {
+//       next("there was an error!");
+//     } else {
+//       res.write("a");
+//     }
+//   }
+//   res.end();
+// });
+
+// // 404 error handle
+// app.use((req, res, next) => {
+//   next("Requested url was not found!");
+// });
+
+// app.use((err, req, res, next) => {
+//   // it has to be last middleware
+//   if (res.headerSent) {
+//     next("there was a problem!");
+//   } else {
+//     if (err.message) {
+//       res.status(500).send(err.message);
+//     } else {
+//       res.send("there was an error");
+//     }
+//   }
+// });
+
+// app.listen(3000, () => {
+//   console.log("listening on port 3000");
+// });
+
+//assynchronous code error handle
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
-app.use('/admin', adminRouter);
-app.use('/', publicRouter);
+app.get("/", [
+  (req, res, next) => {
+    fs.readFile("/file-does-not-exist", (err, data) => {
+      console.log(data);
+      next(err);
+    });
+  },
+  (req, res, next) => {
+    console.log(data.property);
+  },
+]);
+
+app.use((req, res, next) => {
+  console.log("i am not called");
+  next();
+});
+
+app.use((err, req, res, next) => {
+  // it has to be last middleware
+  if (res.headerSent) {
+    next("there was a problem!");
+  } else {
+    if (err.message) {
+      res.status(500).send(err.message);
+    } else {
+      res.send("there was an error");
+    }
+  }
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
